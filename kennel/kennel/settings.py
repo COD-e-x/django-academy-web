922c7f5bc7691079573660b40b924aaf904b48ae
Shamil Lachenilaev <lachen1305@gmail.com>
2025-04-10 18:28:31 +0300

    Add custom login/logout redirect paths for CBV

--- kennel/kennel/settings.py
+++ kennel/kennel/settings.py
@@ -160,6 +160,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
+LOGIN_REDIRECT_URL = "dogs:index"
+LOGOUT_REDIRECT_URL = "dogs:index"
+LOGIN_URL = "/users/"
 
 # Email newsletter
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -174,7 +177,7 @@ EMAIL_SERVER = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
 
-# LOGGING = {0
+# LOGGING = {
 #     "version": 1,
 #     "disable_existing_loggers": False,
 #     "formatters": {
