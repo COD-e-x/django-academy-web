5785d6d462fee7d094784d6e3b3f72badd3d19b3
Shamil Lachenilaev <lachen1305@gmail.com>
2025-04-17 10:45:53 +0300

    Fixed the Redis settings

--- kennel/config/core/signals.py
+++ kennel/config/core/signals.py
@@ -2,7 +2,13 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.files.storage import default_storage
 from django.core.cache import cache
+from django.conf import settings
+
 from dogs.models import Dog, Breed
+
+
+def is_cache_enabled():
+    return not getattr(settings, "CACHE_DISABLED", False)
 
 
 @receiver(post_delete, sender=Dog)
@@ -15,17 +21,19 @@ def delete_photo(sender, instance, **kwargs):
 @receiver([post_save, post_delete], sender=Breed)
 def clear_breed_cache(sender, **kwargs):
     """Очистка кэша породы при изменении/удалении."""
-    cache.delete("breed_list")
+    if is_cache_enabled():
+        cache.delete("breed_list")
 
 
 @receiver([post_save, post_delete], sender=Dog)
 def clear_dog_by_breed_cache(sender, instance, **kwargs):
     """Очистка кэша собак по породе при изменении/удалении."""
-    if instance.breed_id:
+    if instance.breed_id and is_cache_enabled():
         cache.delete(f"dogs_by_breed_{instance.breed_id}")
 
 
 @receiver([post_save, post_delete], sender=Dog)
 def clear_dog_list_cache(sender, **kwargs):
     """Очистка кэша собак при изменении/удалении."""
-    cache.delete(f"dog_list")
+    if is_cache_enabled():
+        cache.delete(f"dog_list")
